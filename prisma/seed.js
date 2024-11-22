300f3d18c7bc21bfc164a54bf96f5b978b4288e2
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-11-22 08:57:11 -0800

    seed the database and add prisma seed to package.json

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,3 +1,18 @@
 const { faker } = require("@faker-js/faker");
 const prisma = require("../prisma");
 
+const seed = async (numProducts = 20) => {
+  const products = Array.from({length: numProducts}, ()=>({
+    title: faker.commerce.productName(),
+    description: faker.lorem.sentences(2),
+    price: Math.floor(Math.random() * 36) + 1
+  }));
+  await prisma.product.createMany({data: products});
+}
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
