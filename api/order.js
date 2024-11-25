a513edf96e0ade05adb0b6cbfcd5acd35423d26e
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-11-24 20:02:52 -0800

    be able to post orders and get orders by their id

--- api/order.js
+++ api/order.js
@@ -16,12 +16,43 @@ router.get("/", authenticate, async (req, res, next)=> {
   }
 });
 
-// router.post("/", authenticate, async (req, res, next) => {
-//   try {
-    
+router.post("/", authenticate, async (req, res, next) => {
+  const {date, note, productIds } = req.body;
+  try {
+    const order = await prisma.order.create({
+      data: {
+        date,
+        note,
+        customerId: req.user.id,
+        items: {
+          connect: productIds.map(id => ({id: parseInt(id)})),
+        },
+      },
+      include: { items: true },
+    });
+    res.status(201).json(order);
+  } catch (e) {
+    res.status(500).json({error: "Failed to make order"});
+    next(e);
+  }
+})
 
-//   } catch (e) {
-//     next(e);
-//   }
-// })
+router.get('/:id', authenticate, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const order = await prisma.order.findUnique({
+      where: { id: parseInt(id) },
+      include: { items: true },
+    });
+    if (!order) return res.status(404).json({ error: 'Order not found' });
+   
+    if (order.customerId !== req.user.id) {
+      return res.status(403).json({ error: 'Forbidden: Not your order' });
+    }
+    res.json(order);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch order' });
+  }
+});
 
